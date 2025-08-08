90c22d01e94499d7337e0a3da2e2e5fc6eefbc1f
aidanashby <aidanashby@gmail.com>
2025-08-08 12:36:41 +0100

    Delete responsive-goodies directory

--- responsive-goodies/admin/js/admin.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    'use strict';
-    
-    $(document).ready(function() {
-        // Admin JavaScript functionality can be added here
-        console.log('Responsive Goodies admin scripts loaded');
-    });
-    
-})(jQuery);
--- responsive-goodies/assets/js/frontend.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    'use strict';
-    
-    $(document).ready(function() {
-        // General frontend JavaScript functionality can be added here
-        console.log('Responsive Goodies frontend scripts loaded');
-    });
-    
-})(jQuery);
